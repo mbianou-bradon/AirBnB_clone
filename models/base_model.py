ff2e62e0c6e3ff5945db649a7dc91c7cef33408f
mbianou-bradon <mbianoubradon2000@gmail.com>
2023-08-12 21:20:30 -0700

    Added file_storage file and test_file_storage and also test_save_reload_base_model

--- models/base_model.py
+++ models/base_model.py
@@ -3,8 +3,12 @@
 
 import uuid
 from datetime import datetime
+from models.engine.file_storage import storage
 
 class BaseModel:
+    """
+    Base model class representing the base attributes and methods for all other models.
+    """
     def __init__(self, *args, **kwargs):
         if kwargs:
             if '__class__' in kwargs:
@@ -18,14 +22,25 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            storage.new(self)
 
     def __str__(self):
+        """
+        Returns a string representation of the BaseModel instance.
+        """
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
+         """
+        Saves the current instance to the storage and updates the 'updated_at' attribute.
+        """
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
+        """
+        Returns a dictionary representation of the BaseModel instance.
+        """
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
         obj_dict['created_at'] = self.created_at.isoformat()
