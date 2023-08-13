a680bb7c65ddb985e003ca4ace49767563025c01
mbianou-bradon <mbianoubradon2000@gmail.com>
2023-08-12 20:48:24 -0700

    Updated base_model to consider dist

--- models/base_model.py
+++ models/base_model.py
@@ -5,10 +5,19 @@ import uuid
 from datetime import datetime
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            if '__class__' in kwargs:
+                kwargs.pop('__class__')
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
